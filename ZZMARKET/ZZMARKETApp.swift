a9b18c1d015ccf944d38ddbce89ac1719a5ec8c6
ZZBAE <jebyun2@gmail.com>
2022-11-12 17:59:41 +0900

    Initial Commit

---
+++ ZZMARKET/ZZMARKETApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZZMARKETApp.swift
+//  ZZMARKET
+//
+//  Created by 변재은 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ZZMARKETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZZMARKETUITests/ZZMARKETUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZZMARKETUITestsLaunchTests.swift
+//  ZZMARKETUITests
+//
+//  Created by 변재은 on 2022/11/12.
+//
+
+import XCTest
+
+final class ZZMARKETUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
